2c9c53ef2fdbae013b0bd3996e3056e62e810bee
benedikt-orsolic <benedikt.orsolic@gmail.com>
2022-11-24 23:13:55 +0100

    Initial nvim setup

---
+++ .config/nvim/init.lua
@@ -0,0 +1,4 @@
+require 'user.settings'
+require 'user.plugins'
+require 'user.telescope'
+require 'user.nvim-tree'
---
+++ .config/nvim/lua/user/plugins.lua
@@ -0,0 +1,58 @@
+-- Bootstrap packer
+local packer_bootstrap = function()
+	local fn = vim.fn
+	local install_path = fn.stdpath('data')..'/site/pack/packer/start/packer.nvim'
+	if fn.empty(fn.glob(install_path)) > 0 then
+		fn.system({
+			'git', 
+			'clonse', 
+			'--depth', 
+			'1', 
+			'https://github.com/wbthomason/packer.nvim', 
+			install_path
+		})
+		print "Packer install"
+		vim.cmd [[packadd packer.nvim]]
+		return true
+	end
+	return false
+end
+
+-- Automaticly run :PackerCompile on plugins.lua update
+vim.cmd([[
+	augroup packer_user_config
+		autocmd!
+		autocmd BufWritePost plugins.lua source <afile> | PackerCompile
+	augroup end
+]])
+
+
+local status_packer, packer = pcall(require, 'packer')
+if not status_packer then
+	return
+end
+
+packer.startup(function(use)
+	use 'wbthomason/packer.nvim'
+	
+
+	-- nvim tree
+	use {
+		'nvim-tree/nvim-tree.lua',
+		requires = {
+			'nvim-tree/nvim-web-devicons',
+		},
+		tag = 'nightly' -- update every week
+	}
+
+
+	use {
+		'nvim-telescope/telescope.nvim',
+		requires = {{ 'nvim-lua/plenary.nvim'}},
+		tag = '0.1.0'
+	}
+	
+	if packer_botstrap then
+		require('packer').sync()
+	end
+end)
