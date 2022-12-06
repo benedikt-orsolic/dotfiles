2de6e4c8f4f79aa4ca8e57fe9286f2ba879c3bae
benedikt-orsolic <benedikt.orsolic@gmail.com>
2022-12-06 22:28:59 +0100

    Update keymaps for formating Autoformating for the file

--- .config/nvim/lua/user/keymaps.lua
+++ .config/nvim/lua/user/keymaps.lua
@@ -1,7 +1,7 @@
-vim.g.mapleader = ' '
+vim.g.mapleader = " "
 function map(mode, lhs, rhs, opts)
 	local options = { noremap = true }
-	if opts then 
+	if opts then
 		options = vim.tbl_extend("force", options, opts)
 	end
 	vim.api.nvim_set_keymap(mode, lhs, rhs, options)
@@ -10,5 +10,10 @@ end
 map("n", "<C-d>", "<C-d>zz")
 map("n", "<C-u>", "<C-u>zz")
 
+map("n", "<C-h>", "<C-w>h")
+map("n", "<C-l>", "<C-w>l")
+
+map("n", "<Leader>f", "<cmd>lua vim.lsp.buf.format({async = true})<cr>")
+
 map("n", "<Leader>e", ":NvimTreeToggle<CR>")
-map("n", "<Leader>f", ":Telescope find_files<CR>")
+map("n", "<Leader>ff", ":Telescope find_files<CR>")
