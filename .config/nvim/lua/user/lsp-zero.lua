76beaac1e4c53cf6b0f71ec6c80da7f86c13c668
benedikt-orsolic <benedikt.orsolic@gmail.com>
2023-03-03 20:59:16 +0100

    Use lsp zero, add options.lua Set up rust analayzer Added virtual text warnings and errors`

---
+++ .config/nvim/lua/user/lsp-zero.lua
@@ -0,0 +1,36 @@
+--local status_ok, lsp_zero = pcall(require, 'lsp-zero')
+--if not status_ok then
+  --return
+--end
+
+local lsp = require('lsp-zero').preset({
+	name = 'minimal',
+	set_lsp_keymaps = true,
+	manage_nvim_cmp = true,
+	suggest_lsp_servers = true,
+})
+
+lsp.setup()
+
+--lsp.ensure_installed({
+--	'tsserver',
+--	'eslint',
+--	'sumneko_lua',
+--	'rust_analyzer'
+--})
+
+vim.diagnostic.config({
+	virtual_text = true,
+	signs = true,
+	update_in_insert = false,
+	underline = true,
+	severity_sort = true,
+	float = {
+		focusable = false,
+		style = 'minimal',
+		border = 'rounded',
+		source = 'always',
+		header = '',
+		prefix = '',
+	}
+})
---
+++ .config/nvim/lua/user/options.lua
@@ -0,0 +1,7 @@
+vim.opt.scrolloff = 10
+
+-- Nvim tree options
+vim.g.loaded_netrw = 1
+vim.g.loaded_netrwPlugin = 1
+vim.g.termguicolors = true
+
