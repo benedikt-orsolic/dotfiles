5ee7b24ddb7c6b3dd26d64767780b7ae456844a3
benedikt-orsolic <benedikt.orsolic@gmail.com>
2023-05-15 23:50:21 +0200

    Add format binding

--- .config/nvim/lua/user/lsp-zero.lua
+++ .config/nvim/lua/user/lsp-zero.lua
@@ -10,6 +10,15 @@ local lsp = require('lsp-zero').preset({
 	suggest_lsp_servers = true,
 })
 
+lsp.on_attach(function(client, bufnr)
+	lsp.default_keymaps({bufer = bufnr})
+	local opts = {buffer = bufnr}
+
+	vim.keymap.set({'n', 'x'}, '<leader>fb', function()
+		vim.lsp.buf.format({async = false, timeout_ms = 10000})
+	end, opts)
+end)
+
 lsp.setup()
 
 --lsp.ensure_installed({
