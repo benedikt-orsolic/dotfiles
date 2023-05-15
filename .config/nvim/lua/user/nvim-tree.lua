0cf93d16f790d2b0ea38e05c111fcc3a007ad070
benedikt-orsolic <benedikt.orsolic@gmail.com>
2023-05-15 23:45:09 +0200

    Use float for nvim tree and fix closeing buffer closes nvim

--- .config/nvim/lua/user/nvim-tree.lua
+++ .config/nvim/lua/user/nvim-tree.lua
@@ -6,16 +6,19 @@ end
 nvim_tree.setup {
 	view = {
 		number = true,
-		relativenumber = true
-	}
-}
+		relativenumber = true,
+		width = 60,
+		float = {
+			enable = true,
+			quit_on_focus_loss = true,
+			open_win_config = {
+				border = "rounded",
+				width = 60,
+				height = 30,
+				row = 1,
+				col = 1,
+			}
+		}
+	},
 
-vim.api.nvim_create_autocmd('BufEnter', {
-	nested = true,
-	callback = function()
-		if #vim.api.nvim_list_wins() == 1
-		    and require('nvim-tree.utils').is_nvim_tree_buf() then
-			vim.cmd 'quit'
-		end
-	end
-})
+}
