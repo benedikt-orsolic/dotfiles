c00eb7cd4e9e36749275494a3d51e9fde9a016d4
benedikt-orsolic <benedikt.orsolic@gmail.com>
2023-05-16 20:20:31 +0200

    Add line wrap for long telescope results Credits; https://github.com/nvim-telescope/telescope.nvim/issues/1958#issuecomment-1133984430

--- .config/nvim/lua/user/telescope.lua
+++ .config/nvim/lua/user/telescope.lua
@@ -3,4 +3,13 @@ if not status_ok then
 	return
 end
 
-telescope.setup {}
+
+telescope.setup {
+	pickers = {
+		find_files = {
+			find_command = { "fd", "-t=f", "-a" },
+			path_display = { "absolute" },
+			wrap_results = true,
+		}
+	}
+}
