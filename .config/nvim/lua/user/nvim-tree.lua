8602ab6a2bbf35d2899bc9ce3821f78a77830e65
benedikt-orsolic <benedikt.orsolic@gmail.com>
2023-05-16 19:55:44 +0200

    Float nvim tree window in the center credit to: https://github.com/MarioCarrion/videos/blob/269956e913b76e6bb4ed790e4b5d25255cb1db4f/2023/01/nvim/lua/plugins/nvim-tree.lua

--- .config/nvim/lua/user/nvim-tree.lua
+++ .config/nvim/lua/user/nvim-tree.lua
@@ -3,22 +3,41 @@ if not status_ok then
 	return
 end
 
+local HEIGHT_RATIO = 0.8
+local WIDTH_RATIO = 0.8
+
 nvim_tree.setup {
 	view = {
 		number = true,
 		relativenumber = true,
-		width = 60,
 		float = {
 			enable = true,
 			quit_on_focus_loss = true,
-			open_win_config = {
-				border = "rounded",
-				width = 60,
-				height = 30,
-				row = 1,
-				col = 1,
-			}
-		}
+			open_win_config = function()
+				local screen_w = vim.opt.columns:get()
+				local screen_h = vim.opt.lines:get() - vim.opt.cmdheight:get()
+
+				-- Ratio
+				local window_w = math.floor(screen_w * WIDTH_RATIO)
+				local window_h = math.floor(screen_h * HEIGHT_RATIO)
+
+				local center_x = (screen_w - window_w) / 2
+				local center_y = ((screen_h - window_h) / 2) - vim.opt.cmdheight:get()
+
+
+				return {
+					border = "rounded",
+					relative = "editor",
+					row = center_y,
+					col = center_x,
+					width = window_w,
+					height = window_h,
+				}
+			end,
+		},
+		width = function()
+			return math.floor(vim.opt.columns:get() * WIDTH_RATIO)
+		end,
 	},
 
 }
